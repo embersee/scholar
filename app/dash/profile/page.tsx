7a2b9147bbd5c7a1b077b810635abbaf55d166a8
AJIEXAHDPO <alesha.iv03@gmail.com>
2024-01-23 10:10:12 +0300

    aftermerge fixes

--- app/dash/profile/page.tsx
+++ app/dash/profile/page.tsx
@@ -4,8 +4,8 @@ import { getUserAuth } from "@/server/auth";
 import ProfileInfo from "@/components/profile/ProfileInfo";
 import ApprtsList from "@/components/apprts/List";
 import Container from "@/components/ui/container";
-// import ProfileForm from "@/components/profile/Form";
-// import { GetUser } from "@/server/schema/user";
+ import ProfileForm from "@/components/profile/Form";
+ import { GetUser } from "@/server/schema/user";
 
 export default async function Profile() {
 
@@ -14,8 +14,8 @@ export default async function Profile() {
   const user = await api.user.getUserById.query({ telegram_id: session.user.id });
   if (!user) return null;
   const apprts = await api.apprts.getApprenticeships.query();
-  //const data = await api.institutions.getInstitutions.query();
-  //const user = (await api.user.getAuthedUserWithInstitution.query()) as GetUser;
+  const data = await api.institutions.getInstitutions.query();
+  const user_1 = (await api.user.getAuthedUserWithInstitution.query()) as GetUser;
 
   return (
     <>
@@ -23,14 +23,15 @@ export default async function Profile() {
         title="Profile"
         description="Create or select which bot to manage here."
       ></Heading>
-      {/*<div className="form">
-        <ProfileForm user={user} institutions={data} />
-      </div>*/}
       <div className="grid grid-flow-row md:grid-flow-col gap-3">
         <ProfileInfo user={user} />
         <Container className="justify-center">
           <ApprtsList apprts={apprts} />
         </Container>
+        
+        <div className="form">
+          <ProfileForm user={user_1} institutions={data} />
+        </div>
       </div>
     </>
   );
