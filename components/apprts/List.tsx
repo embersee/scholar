3d73c69a7ca0565b39065d2ba8d2aa4afd8a1b30
AJIEXAHDPO <alesha.iv03@gmail.com>
2024-01-29 17:35:42 +0300

    changed apprenticeship list displaying

--- components/apprts/List.tsx
+++ components/apprts/List.tsx
@@ -1,6 +1,9 @@
 import { GetApprenticeship } from "@/server/schema/apprenticeship";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { api } from "@/trpc/server";
+import { GetUser } from "@/server/schema/user";
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "../ui/drawer";
 
 type Props = {
   apprts: GetApprenticeship;
@@ -25,9 +28,57 @@ export default function ApprtsList(props: Props) {
 
   return (
     <ul>
-      {props.apprts.map((v, i) => (
-        <li key={i}>{i}</li>
-      ))}
+      {props.apprts.map(async (apprt, i) => {
+        const user = (await api.user.getAuthedUserWithInstitution.query()) as GetUser;
+        if (!user) return null;
+        //const user1 = {...user, FIO: "Ivanov Alexey Vladimirovich"};
+        return (
+          <li key={i}><ApprenticeShip user={user} apprt={apprt} /></li>
+        )
+      }
+      )}
     </ul>
   );
 }
+
+type apprt = GetApprenticeship[0];
+
+const ApprenticeShip = async ({ user, apprt }: { user: GetUser, apprt: apprt }) => {
+  const apprtsTypes = await api.apprts.getTypes.query();
+  return (
+    <Drawer>
+      <DrawerTrigger className="w-[100%]">
+        <Button variant="ghost" className="flex justify-between w-[100%] px-10 h-[64px] items-center">
+          <div className="flex flex-col items-start">
+            <div>{`@${user?.username}`}</div>
+            <div className="hidden md:block">{user?.FIO}</div>
+          </div>
+          <div>{apprt.apprenticeshipTypeId}</div>
+          <div>{`${apprt.signed}`}</div>
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent className="flex flex-column justify-center items-center">
+        <DrawerHeader>
+          <DrawerTitle>Student Info</DrawerTitle>
+          <DrawerDescription>View details</DrawerDescription>
+        </DrawerHeader>
+        <div className="flex flex-row items-center justify-center">
+          <div className="p-10">FIO</div>
+          <div className="p-10">Dates</div>
+        </div>
+        <DrawerFooter>
+          <DrawerClose>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+//enum Apprt_status {
+//  waiting
+//  closed
+//  approved
+//  failed
+//}
